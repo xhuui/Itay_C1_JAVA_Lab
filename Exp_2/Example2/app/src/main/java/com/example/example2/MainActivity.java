c35e1dd5145832cb676fe660385d0a973bde9343
xhuui <65158386+xhuui@users.noreply.github.com>
2024-07-27 11:43:58 +0300

    a

---
+++ Exp_2/Example2/app/src/main/java/com/example/example2/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.example2;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private TextView textView;
+    private Button button;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        editText = findViewById(R.id.edtEnterText);
+        textView = findViewById(R.id.txtShowText);
+        button = findViewById(R.id.btnMagic);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(editText.getText());
+            }
+        });
+    }
+
+}
--- Exp_2/Example_01-master/README.md
+++
@@ -1,4 +0,0 @@
-# Example_01
-Example to android experiment
-
-https://nssl.technion.ac.il/exp/android/guide.html
